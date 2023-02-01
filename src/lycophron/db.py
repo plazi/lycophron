d7e59f92e8ef96fb23d42ac16f520b288b00bee9
Manuel Alejandro <mumox7@gmail.com>
2023-02-01 14:55:56 +0100

    init: implemented init command

--- src/lycophron/db.py
+++ src/lycophron/db.py
@@ -10,29 +10,50 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils.functions import create_database, database_exists, drop_database
-from config import SQLALCHEMY_DATABASE_URI
-from errors import DatabaseAlreadyExists
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+import logging
+
+from .errors import DatabaseAlreadyExists
+from .app import app
 
 Model = declarative_base()
+logger = logging.getLogger(__name__)
 
 
 class LycophronDB(object):
-    def __init__(self) -> None:
-        self.engine = create_engine(SQLALCHEMY_DATABASE_URI)
-        self.session = sessionmaker(bind=self.engine)
+    """Manages a lycophron DB."""
+
+    def __init__(self, uri) -> None:
+        self.engine = create_engine(uri)
+        _session_factory = sessionmaker(bind=self.engine)
+        self.session = scoped_session(_session_factory)
 
     def init_db(self) -> None:
         """Initializes the lycophron database."""
         if self.database_exists():
-            raise DatabaseAlreadyExists("A database is already created with the given.")
+            raise DatabaseAlreadyExists(
+                "A database is already created."
+            )
         create_database(self.engine.url)
         Model.metadata.create_all(self.engine)
+        logger.info("Database created!")
 
     def recreate_db(self) -> None:
         """Drop and recreate database."""
+        logger.warn("Recreating database. THIS WILL DESTROY THE CURRENT DATABASE, PROCEED WITH CAUTION.")
         drop_database(self.engine.url)
         create_database(self.engine.url)
         Model.metadata.create_all(self.engine)
 
     def database_exists(self) -> bool:
+        """Check if database exists
+
+        :return: True if exists, False otherwise
+        :rtype: bool
+        """
         return database_exists(self.engine.url)
+
+
+db_uri = app.config["SQLALCHEMY_DATABASE_URI"]
+db = LycophronDB(uri=db_uri)
--- src/lycophron/project.py
+++ src/lycophron/project.py
@@ -3,65 +3,84 @@ import os
 
 import click
 
-from lycophron import models
+from .errors import DatabaseAlreadyExists, ErrorHandler
+from .loaders import LoaderFactory
 
 
 class Project:
-    def __init__(self, name):
-        self.name = name
-        self.current_dir = os.getcwd()
-        self.project_dir = self.current_dir + "/" + self.name
-        self.files_dir = self.project_dir + "/" + "files"
-        self.db_dir = self.project_dir + "/app.db"
-        self.csv_dir = self.project_dir + "/input.csv"
+    def __init__(self, project_folder=os.getcwd()):
+        self.project_folder = project_folder
 
-    def create_db(self):
-        """Create a database"""
-        models.create_all()
-        click.secho("DB Created!", fg="green")
+    def load_file(self, filename):
+        print("Loading file!")
+        return
+        file_path = os.path.join(self.project_folder, filename)
+        loader = LoaderFactory.create_loader(filename)
+        loaded_data = loader.load(file_path)
+        deserialized_data = loader.deserializer.deserialize(loaded_data)
 
-    def create_csv(self):
-        """Create a CSV"""
-        header = [
-            "id",
-            "doi",
-            "deposit_id",
-            "title",
-            "description",
-            "creators.name",
-            "creators.affiliation",
-            "creators.orcid",
-            "dwc:kingdom",
-            "dwc:order",
-            "dwc:family",
-            "dwc:collectionCode",
-            "related_identifiers.identifier",
-            "related_identifiers.relation",
-            "files",
-        ]
+    # def create_csv(self):
+    #     """Create a CSV"""
+    #     header = [
+    #         "id",
+    #         "doi",
+    #         "deposit_id",
+    #         "title",
+    #         "description",
+    #         "creators.name",
+    #         "creators.affiliation",
+    #         "creators.orcid",
+    #         "dwc:kingdom",
+    #         "dwc:order",
+    #         "dwc:family",
+    #         "dwc:collectionCode",
+    #         "related_identifiers.identifier",
+    #         "related_identifiers.relation",
+    #         "files",
+    #     ]
 
-        with open(self.csv_dir, "w", encoding="UTF8") as f:
-            writer = csv.writer(f)
+    #     with open(self.csv_dir, "w", encoding="UTF8") as f:
+    #         writer = csv.writer(f)
 
-            writer.writerow(header)
+    #         writer.writerow(header)
 
-        click.secho("CSV Created!", fg="green")
+    #     click.secho("CSV Created!", fg="green")
 
-    def create_folder(self, create_files_dir="False"):
-        """Create a folder"""
-        try:
-            if create_files_dir == True:
-                os.mkdir(self.files_dir)
-            else:
-                os.mkdir(self.name)
+    # def create_folder(self, create_files_dir="False"):
+    #     """Create a folder"""
+    #     try:
+    #         if create_files_dir == True:
+    #             os.mkdir(self.files_dir)
+    #         else:
+    #             os.mkdir(self.name)
 
-            click.secho("Folder Created!", fg="green")
-        except FileExistsError:
-            click.secho("The folder already exists!", fg="green")
+    #         click.secho("Folder Created!", fg="green")
+    #     except FileExistsError:
+    #         click.secho("The folder already exists!", fg="green")
 
     def initialize(self):
         """Initialize the project"""
-        self.create_folder()
-        self.create_folder(True)
-        self.create_csv()
-        self.create_db()
+        # self.create_folder()
+        # self.create_folder(True)
+        # self.create_csv()
+        from .db import db
+
+        try:
+            db.init_db()
+        except DatabaseAlreadyExists as e:
+            ErrorHandler.handle_error(e)
+
+    def recreate_project(self):
+        from .db import db
+
+        db.recreate_db()
+
+    
+    def validate_project(self):
+        raise NotImplementedError("Project validation not implemented yet")
+
+    
+    def is_project_initialized(self):
+        from .db import db
+        
+        return db.database_exists()
